e0afb25e7d05edc8b2e2cad46e711fc964bbab5d
mh <mh@immerda.ch>
2008-03-09 16:51:52 +0000

    added fail2ban module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,33 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/fail2ban/manifests/init.pp - manage fail2ban stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "fail2ban": }
 
-class skeleton {
 
+class fail2ban {
+    case $operatingsystem {
+        gentoo: {include fail2ban::gentoo}
+        default: {include fail2ban::base}
+    }
+}
+
+class fail2ban::base {
+
+    package{fail2ban:
+        ensure => installed,
+    }
+
+    service{fail2ban:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+    }
+
+}
+
+class fail2ban::gentoo inherits fail2ban::base {
+    Package[fail2ban]{
+        category => 'net-analyzer',
+    }
 }
