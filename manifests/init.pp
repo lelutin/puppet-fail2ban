69ca0feb4a331cad5cc5d9b902a8b475f5379550
mh <mh@immerda.ch>
2009-11-14 14:51:15 +0100

    update to 0.25.x style

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,47 +2,9 @@
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "fail2ban": }
-
-
 class fail2ban {
     case $operatingsystem {
         gentoo: {include fail2ban::gentoo}
         default: {include fail2ban::base}
-    }
-}
-
-class fail2ban::base {
-
-    package{fail2ban:
-        ensure => installed,
-    }
-
-    service{fail2ban:
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-    }
-
-}
-
-class fail2ban::gentoo inherits fail2ban::base {
-    Package[fail2ban]{
-        category => 'net-analyzer',
-    }
-    #conf.d file if needed
-    Service[fail2ban]{
-       require +> File["/etc/conf.d/fail2ban"],
-    }
-    file { "/etc/conf.d/fail2ban":
-        owner => "root",
-        group => "0",
-        mode  => 644,
-        ensure => present,
-        source => [
-            "puppet://$server/files/fail2ban/conf.d/${fqdn}/fail2ban",
-            "puppet://$server/files/fail2ban/conf.d/fail2ban",
-            "puppet://$server/modules/fail2ban/conf.d/fail2ban"
-        ]
     }
 }
