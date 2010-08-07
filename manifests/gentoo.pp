5f8a118586d71c8026f0af5b031c673c60003d44
mh <mh@immerda.ch>
2010-08-07 02:12:15 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/gentoo.pp
+++ manifests/gentoo.pp
@@ -5,9 +5,9 @@ class fail2ban::gentoo inherits fail2ban::base {
     file { "/etc/conf.d/fail2ban":
         ensure => present,
         source => [
-            "puppet://$server/modules/site-fail2ban/conf.d/${fqdn}/fail2ban",
-            "puppet://$server/modules/site-fail2ban/conf.d/fail2ban",
-            "puppet://$server/modules/fail2ban/conf.d/fail2ban"
+            "puppet:///modules/site-fail2ban/conf.d/${fqdn}/fail2ban",
+            "puppet:///modules/site-fail2ban/conf.d/fail2ban",
+            "puppet:///modules/fail2ban/conf.d/fail2ban"
         ],
         require => Package['fail2ban'],
         notify => Service['fail2ban'],
