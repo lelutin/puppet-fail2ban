7096f650cb11248145045a3863708e552ff6d8f7
Lucas Clair <lclair@plusline.net>
2015-12-21 13:44:34 +0100

    Update apache_overflows.pp

--- manifests/jail/apache_overflows.pp
+++ manifests/jail/apache_overflows.pp
@@ -9,12 +9,19 @@ class fail2ban::jail::apache_overflows (
     default      => $maxretry
   }
 
+  $logpath = $::osfamily ? {
+    'Debian' => '/var/log/apache*/*error.log',
+    'Gentoo' => '/var/log/apache*/*error.log',
+    'RedHat' => '/var/log/httpd/*error.log',
+    default  => fail ("Unsupported Operating System family: ${::osfamily}"),
+  }
+
   # Use default apache-overflows filter from debian
   fail2ban::jail { 'apache-overflows':
     enabled  => true,
     port     => 'http,https',
     filter   => 'apache-overflows',
-    logpath  => '/var/log/apache*/*error.log',
+    logpath  => $logpath,
     maxretry => $real_maxretry,
     findtime => $findtime,
     ignoreip => $ignoreip,
