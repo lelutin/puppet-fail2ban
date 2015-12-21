d23cd0727523524accf2d1a29218fb790fb39518
Lucas Clair <lclair@plusline.net>
2015-12-21 14:18:52 +0100

    Update pure_ftpd.pp

--- manifests/jail/pure_ftpd.pp
+++ manifests/jail/pure_ftpd.pp
@@ -8,13 +8,20 @@ class fail2ban::jail::pure_ftpd (
     'usedefault' => '6',
     default      => $maxretry
   }
+  
+  $logpath = $::osfamily ? {
+    'Debian' => '/var/log/auth.log',
+    'Gentoo' => '/var/log/auth.log',
+    'RedHat' => '%(pureftpd_log)s',
+    default  => fail ("Unsupported Operating System family: ${::osfamily}"),
+  }
 
   # Use default pure-ftpd filter from debian
   fail2ban::jail { 'pure-ftpd':
     enabled  => true,
     port     => 'ftp,ftp-data,ftps,ftps-data',
     filter   => 'pure-ftpd',
-    logpath  => '/var/log/auth.log',
+    logpath  => $logpath,
     maxretry => $real_maxretry,
     findtime => $findtime,
     ignoreip => $ignoreip,
