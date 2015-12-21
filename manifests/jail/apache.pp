37b35bdd1926dccf964e58024501a72cfb576171
Lucas Clair <lclair@plusline.net>
2015-12-21 13:43:02 +0100

    Update apache.pp

--- manifests/jail/apache.pp
+++ manifests/jail/apache.pp
@@ -9,12 +9,19 @@ class fail2ban::jail::apache (
     default      => $maxretry
   }
 
+  $logpath = $::osfamily ? {
+    'Debian' => '/var/log/apache*/*error.log',
+    'Gentoo' => '/var/log/apache*/*error.log',
+    'RedHat' => '/var/log/httpd/*error.log',
+    default  => fail ("Unsupported Operating System family: ${::osfamily}"),
+  }
+  
   # Use default apache-auth filter from debian
   fail2ban::jail { 'apache':
     enabled  => true,
     port     => 'http,https',
     filter   => 'apache-auth',
-    logpath  => '/var/log/apache*/*error.log',
+    logpath  => $logpath,
     maxretry => $real_maxretry,
     findtime => $findtime,
     ignoreip => $ignoreip,
