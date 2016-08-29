522ff8ef8726387df8a8032cee2593a1ec37e74c
Luis Silva <luis_silva@harvard.edu>
2016-08-29 12:37:37 -0400

    no need to split ip list

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,8 +17,7 @@ class fail2ban::config {
   $action          = $fail2ban::action
   $persistent_bans = $fail2ban::persistent_bans
   
-  $ips = split($ignoreip, " ")
-  validate_ip_address($ips)
+  validate_ip_address($ignoreip)
   validate_integer($bantime, $findtime, $maxretry)
   validate_bool($persistent_bans)
   validate_re($usedns, [ 'yes', 'no', 'warn' ], 'usedns value must be yes, no or warn.')
