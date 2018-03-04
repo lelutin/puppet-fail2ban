f239abe568a0c2d9a66c70b265b9a5afa0612952
Gabriel Filion <gabster@lelutin.ca>
2018-03-04 02:59:15 -0500

    use contain instead of anchors
    ... just because it's the "new way of doing this". it takes up more
    lines so I'm not super happy about this -- but with puppet you never
    know when not following the next best practice is going to get you
    behind on deprecations.

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,10 +25,12 @@ class fail2ban (
   Boolean $persistent_bans = false,
 ) {
 
-  anchor { 'fail2ban::begin': }
-  -> class { 'fail2ban::install': }
-  -> class { 'fail2ban::config': }
-  ~> class { 'fail2ban::service': }
-  -> anchor { 'fail2ban::end': }
+  contain fail2ban::install
+  contain fail2ban::config
+  contain fail2ban::service
+
+  Class['fail2ban::install']
+  -> Class['fail2ban::config']
+  ~> Class['fail2ban::service']
 
 }
