cdab293c8580606eecc16c34d3f54c8973207f1c
Gabriel Filion <gabster@lelutin.ca>
2019-07-26 00:49:47 -0400

    import template formatted for jessie's fail2ban 0.8
    This should mostly bring jessie support back, as long as folks use
    puppet 4.x (from backports), or more recent versions from upstream.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,15 @@ class fail2ban::params {
 
   case $facts['os']['family'] {
     'Debian': {
-      $jail_conf_template = "${module_name}/debian/jail.conf.erb"
+      if $facts['os']['release']['major'] == 8 {
+        # Keep this special case so that jessie hosts with puppet 4.x from
+        # backports can still configure fail2ban 0.8 as distributed in that
+        # release.
+        $jail_conf_template = "${module_name}/debian/jessie-jail.conf.erb"
+      }
+      else {
+        $jail_conf_template = "${module_name}/debian/jail.conf.erb"
+      }
     }
     'RedHat': {
       $jail_conf_template = "${module_name}/rhel/jail.conf.erb"
