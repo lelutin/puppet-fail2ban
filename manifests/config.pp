280ccf4f863709fc34bba75d7f738ddc99c60f05
Gabriel Filion <gabster@lelutin.ca>
2019-07-26 01:56:01 -0400

    override some default values for debian jessie

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,7 +9,29 @@
 #
 class fail2ban::config {
 
-  $loglvl = $fail2ban::loglvl
+  # This variable is determined here since it overrides the normally permitted
+  # values.
+  if $facts['os']['release']['major'] == '8' {
+    # fail2ban 0.8 had integer loglevels, whereas 0.9 has symbolic names for
+    # levels.
+    #
+    # Levels CRITICAL and NOTICE only exist in fail2ban 0.9. We'll make them
+    # correspond to something that should be about the same level of
+    # information on 0.8
+    $int_levels = {
+      'CRITICAL' => 1,
+      'ERROR'    => 1,
+      'WARN'     => 2,
+      'NOTICE'   => 3,
+      'INFO'     => 3,
+      'DEBUG'    => 4,
+    }
+    $loglvl = $int_levels[$fail2ban::loglvl]
+  }
+  else {
+    $loglvl = $fail2ban::loglvl
+  }
+
   $logtarget = $fail2ban::logtarget
   $syslogsocket = $fail2ban::syslogsocket
   $socket = $fail2ban::socket
@@ -39,6 +61,15 @@ class fail2ban::config {
   }
 
 
+  # Non-trivial override of default logpath value for jessie when param not set
+  # fail2ban 0.8 expects to have logpath defined somewhere..
+  if ($facts['os']['release']['major'] == '8' ) and (! $fail2ban::logpath) {
+    $logpath = '/var/log/messages'
+  }
+  else {
+    $logpath = $fail2ban::logpath
+  }
+
   $persistent_bans = $fail2ban::persistent_bans
 
   $enabled = $fail2ban::enabled
@@ -60,7 +91,6 @@ class fail2ban::config {
   $protocol = $fail2ban::protocol
   $action = $fail2ban::action
   $usedns = $fail2ban::usedns
-  $logpath = $fail2ban::logpath
   $logencoding = $fail2ban::logencoding
   $failregex = $fail2ban::failregex
   $ignoreregex = $fail2ban::ignoreregex
