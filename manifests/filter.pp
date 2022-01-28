5e3a59e27d5dbf426e60a4a34e0660697ca0e6ce
Gabriel Filion <gabster@lelutin.ca>
2022-01-27 23:02:41 -0500

    Teach filter the missing parameters it can use
    Most of those options could previously be configured with the
    additional_defs array hack, but having them present in the code makes it
    possible to document what they do.
    
    additional_defs is still useful since users can define arbitrary
    variables that'll be used in other options.
    
    The init section is new however and was not possible to configure with
    additional_defs since it would have placed failregex and ignoreregex in
    the Init section.

--- manifests/filter.pp
+++ manifests/filter.pp
@@ -11,7 +11,7 @@
 # @api public
 #
 # @see https://fail2ban.readthedocs.io/en/latest/filters.html
-#
+# @see https://github.com/fail2ban/fail2ban/blob/0.11/man/jail.conf.5 jail.conf(5)
 #
 # @example defining filter for jenkins
 #   fail2ban::filter { 'jenkins':
@@ -35,13 +35,6 @@
 #   Whether the resources should be installed or removed.
 # @param config_file_mode
 #   Permission mode given to the filter file created by this defined type.
-# @param ignoreregexes
-#   List of Python regular expressions that should prevent a log line from
-#   being considered for banning. If a line matches regular expressions
-#   contained in this parameter, they are ignored even though they would have
-#   matched a failregex. Each item in the list is placed on its own line. Lines
-#   starting with the second one are prepended with spaces so that the regular
-#   expressions line up with the beginning of the first one.
 # @param includes
 #   List of files to include before considering the rest of the filter
 #   definition. These files can declare variables used by the filter to set
@@ -53,24 +46,57 @@
 #   definition section, for anything that didn't fit in other parameters. Each
 #   item in the list is output on its own line in the filter file. No syntax
 #   checking is done.
+# @param prefregex
+#   If this is set, it contains a regular expression that should be used to
+#   parse (after datepattern found a match) a common part to all messages that
+#   can then match a smaller failregex or ignoreregex. If this regex does not
+#   match, then failregex or ignoreregex are not even tried.
+# @param ignoreregexes
+#   List of Python regular expressions that should prevent a log line from
+#   being considered for banning. If a line matches regular expressions
+#   contained in this parameter, they are ignored even though they would have
+#   matched a failregex. Each item in the list is placed on its own line. Lines
+#   starting with the second one are prepended with spaces so that the regular
+#   expressions line up with the beginning of the first one.
+# @param maxlines
+#   Maximum number of lines that fail2ban should buffer for matching
+#   multi-line regexes.
+# @param datepattern
+#   Custom date pattern/regex for the log file. This is useful if dates use a
+#   non-standard formatting.
+# @param journalmatch
+#   If the log backend is set to systemd, this specifies a matching pattern to
+#   filter journal entries.
 #
 define fail2ban::filter (
   Array[String, 1] $failregexes,
   Enum['present', 'absent'] $ensure = 'present',
   String           $config_file_mode = '0644',
-  Array[String, 0] $ignoreregexes = [],
+  # general configuration
+  Array[String]    $init = [],
   Array[String, 0] $includes = [],
   Array[String, 0] $includes_after = [],
-  Array[String, 0] $additional_defs = []
+  # main filter definition
+  Array[String, 0] $additional_defs = [],
+  Optional[String] $prefregex = undef,
+  Array[String, 0] $ignoreregexes = [],
+  Optional[Integer[1]] $maxlines = undef,
+  Optional[String] $datepattern = undef,
+  Optional[String] $journalmatch = undef,
 ) {
   include fail2ban::config
 
   $filter_options = {
-    failregexes     => $failregexes,
-    ignoreregexes   => $ignoreregexes,
+    init            => $init,
     includes        => $includes,
     includes_after  => $includes_after,
     additional_defs => $additional_defs,
+    prefregex       => $prefregex,
+    failregexes     => $failregexes,
+    ignoreregexes   => $ignoreregexes,
+    maxlines        => $maxlines,
+    datepattern     => $datepattern,
+    journalmatch    => $journalmatch,
   }
 
   file { "/etc/fail2ban/filter.d/${name}.conf":
