6713a66c1a031045fe36f57ae455f6e496e7846d
Gabriel Filion <gabster@lelutin.ca>
2024-08-24 15:46:56 -0400

    Remove dummy apt update in example file
    I worked a bit more on my local dev setup and now I have something that
    starts a container and automatically runs apt-get update in it.
    
    So the ugly workaround for my local tests is not needed anymore in the
    example file.

--- examples/init.pp
+++ examples/init.pp
@@ -1,11 +1,4 @@
 # This test file runs just the most basic fail2ban setup
-
-# This is required when running tests with the puppet/puppet-agent-ubuntu
-# docker image -- otherwise fail2ban fails to install.
-exec { 'update apt archive':
-  command => '/usr/bin/apt-get update',
-}
-
 class { 'fail2ban':
 }
 
